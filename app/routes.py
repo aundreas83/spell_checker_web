bea4f9d9692193824ba7ce1378074b1e676cf92d
aundreas83 <aundreas83@gmail.com>
2019-10-17 23:31:43 -0400

    added id to form fields

--- app/routes.py
+++ app/routes.py
@@ -54,6 +54,6 @@ def register():
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('Congratulations, you are now a registered user!')
+        flash('S')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
