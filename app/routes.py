6ed7908b7021dfc7f7a2d8a196b7acaa4b9d6bf5
aundreas83 <aundreas83@gmail.com>
2019-10-21 14:03:50 -0400

    fixed

--- app/routes.py
+++ app/routes.py
@@ -26,6 +26,13 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+@app.route('/spell_check')
+def spell_checker():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    form = Spell_Checker()
+    return render_template('spell_check.html', title="Spell Checker", form=form)
+
 @app.route('/logout')
 def logout():
     logout_user()
@@ -35,17 +42,6 @@ def logout():
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
     return render_template("index.html", title='Home Page')
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -58,5 +54,5 @@ def register():
         user.set_password(form.pword.data)
         db.session.add(user)
         db.session.commit()
-        flash(Markup('<li id="result">success</li>'))
+        flash(Markup('<li id="success">success</li>'))
     return render_template('register.html', title='Register', form=form)
